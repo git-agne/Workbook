f4d3d03e6b9ae7953d64b7f2b8e7260fc391db15
Agnese <ag.devetta@gmail.com>
2021-06-30 15:59:11 +0200

    Aggiunta

--- src/ires/corso/part2/todo/ToDoListManager.java
+++ src/ires/corso/part2/todo/ToDoListManager.java
@@ -4,21 +4,7 @@ import java.time.LocalDate;
 
 public class ToDoListManager
 {
-    // Classe ToDoList ??
-
     public static void main(String[] args) {
-        /*
-        Menu m = new Menu();
 
-        m.printMenu();
-
-         */
-
-        ToDo td1 = new ToDo("Spesa", "xx", LocalDate.of(2021, 6, 06), LocalDate.of(2022, 9, 10));
-        ToDo td3 = new ToDo("Spesa", "xx", LocalDate.of(2020, 6, 06), LocalDate.of(2022, 9, 10));
-        //ToDo td2 = td1.cloneForUpdate();
-
-        System.out.println(td1.getId().toString());
-        System.out.println(td3.getId().toString());
     }
 }
